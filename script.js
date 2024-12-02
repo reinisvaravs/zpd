0ac9d77491f7fe9cf4e70243d0dd5f426e11be07
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-12-02 22:12:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function simulateSubmission() {
+    alert('Šī bija tikai simulācija. Dati netiek saglabāti.');
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('survey').style.display = 'block';
+}
